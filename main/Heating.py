45203d504ae28b38f2c0b0ead628820948c480e0
Lars Bremer <info@larsbremer.de>
2016-05-25 22:46:19 +0200

    initial commit

---
+++ main/Heating.py
@@ -0,0 +1,16 @@
+'''
+Created on May 25, 2016
+
+@author: larsbremer
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+
+        print "Hello World"
+        
