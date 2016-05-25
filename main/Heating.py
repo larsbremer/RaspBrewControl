7cf87f147fd61c42bc836dc750cdcbbfd1bff038
Lars Bremer <info@larsbremer.de>
2016-05-25 23:01:46 +0200

    third commit

--- main/Heating.py
+++ main/Heating.py
@@ -4,13 +4,9 @@ Created on May 25, 2016
 @author: larsbremer
 '''
 
-class MyClass(object):
-    '''
-    classdocs
-    '''
-
-
-    def __init__(self, params):
-
-        print "Hello World"
-        
+   
+def main():
+    print 'Hello, world!'
+ 
+if __name__ == '__main__':
+    main()
