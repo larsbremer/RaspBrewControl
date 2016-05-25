e930deb6c202e8f7b68e154eab40fff64e24d432
Lars Bremer <info@larsbremer.de>
2016-05-25 23:44:54 +0200

    added while loop

--- main/Heating.py
+++ main/Heating.py
@@ -4,9 +4,23 @@ Created on May 25, 2016
 @author: larsbremer
 '''
 
+import time
+import RPi.GPIO as GPIO
    
 def main():
     print 'Hello, world!'
+    
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(7, GPIO.OUT)
+    
+    while True:
+        
+        time.sleep( 1 )
+        GPIO.output(7, 0) 
+        time.sleep( 1 )
+        GPIO.output(7, 1)   
+        
+        
  
 if __name__ == '__main__':
     main()
